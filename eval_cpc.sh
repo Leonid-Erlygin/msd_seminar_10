deded3519a232bb07a7ca93431e35213c3940057
erlygin <erlygin>
2022-11-30 17:48:50 +0000

    test

---
+++ eval_cpc.sh
@@ -0,0 +1,1 @@
+python cpc/eval/linear_separability.py /app/data/LibriSpeech /app/data/LibriSpeech/train_split_small.txt /app/data/LibriSpeech/test_split_small.txt /app/data/checkpoints/60k_epoch4-d0f474de.pt --pathCheckpoint /app/data/checkpoints/ --pathPhone /app/data/converted_aligned_phones.txt
